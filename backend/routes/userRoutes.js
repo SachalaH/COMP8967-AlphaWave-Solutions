6faa06b96edadd764af6811ee1357aa15d4ce62a
Harsh Sachala <sach46229@gmail.com>
2024-06-12 15:56:47 -0400

    initial frontend setup

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.get("/logout", logout);
+router.get("/getuser", protect, getUser);
+router.get("/loggedin", loginStatus);
+router.patch("/updateuser", protect, updateUser);
+
+module.exports = router;

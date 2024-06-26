b332868b3e347a911239ba06a302eb11d0e9128e
Harsh Sachala <sach46229@gmail.com>
2024-06-25 23:41:25 -0400

    added new routes and their controllers

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,20 @@
 const express = require("express");
-const { registerUser, loginUser, logoutUser } = require("../controllers/user");
+const {
+  registerUser,
+  loginUser,
+  logoutUser,
+  getUser,
+  loginStatus,
+  updateUser,
+} = require("../controllers/user");
+const authorize = require("../middlewares/auth");
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/logout", logoutUser);
+router.get("/getuser", authorize, getUser);
+router.get("/loginstatus", loginStatus);
+router.patch("/updateuser", authorize, updateUser);
 
 module.exports = router;

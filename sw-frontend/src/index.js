491b37817f0c68f474f9ff88aadf3fbf282f2a44
smita172 <smitakhristi48@gmail.com>
2024-07-02 12:52:56 -0400

    redux

--- sw-frontend/src/index.js
+++ sw-frontend/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';  
 import './index.css';
 import App from './App';
+import {store} from "./redux/store";
+import {Provider} from "react-redux";
 
 
 const container = document.getElementById('root');
@@ -9,6 +11,8 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>
 );
